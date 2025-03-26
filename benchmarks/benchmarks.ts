17d93d1a8f2197ce108d69372913567bd75ad7b2
gkapelyushok <kapgosha@gmail.com>
2025-03-26 20:55:18 +0300

    feat: benchmarks

---
+++ benchmarks/benchmarks.ts
@@ -0,0 +1,49 @@
+import {strict as assert} from "assert"
+
+import {generateResults, printBenchmarkTable} from "../utils/gas"
+import benchmarkResults from "./results_gas.json"
+import {
+    runTransferBenchmark,
+    runMintBenchmark,
+    runBurnBenchmark,
+    runDiscoveryBenchmark,
+    runReportBalanceBenchmark,
+} from "./environment"
+
+const main = async () => {
+    const results = generateResults(benchmarkResults)
+    const expectedResult = results.at(-1)!
+    // TODO
+    const gasUsedForTransfer = await runTransferBenchmark()
+    assert.equal(gasUsedForTransfer, expectedResult.gas["transfer"])
+
+    const gasUsedForMint = await runMintBenchmark()
+    assert.equal(gasUsedForMint, expectedResult.gas["mint"])
+
+    const gasUsedForBurn = await runBurnBenchmark()
+    assert.equal(gasUsedForBurn, expectedResult.gas["burn"])
+
+    const gasUsedForDiscovery = await runDiscoveryBenchmark()
+    assert.equal(gasUsedForDiscovery, expectedResult.gas["discovery"])
+
+    const gasUsedForReportBalance = await runReportBalanceBenchmark()
+    assert.equal(gasUsedForReportBalance, expectedResult.gas["reportBalance"])
+
+    assert.equal(
+        gasUsedForTransfer +
+            gasUsedForMint +
+            gasUsedForBurn +
+            gasUsedForDiscovery +
+            gasUsedForReportBalance,
+        expectedResult.summary,
+    )
+
+    if (process.env.PRINT_TABLE === "true") {
+        printBenchmarkTable(results, undefined, {
+            implementationName: "Tact Jetton",
+            printMode: "full",
+        })
+    }
+}
+
+void main()
